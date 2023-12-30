35969c5c79d5d0e6a4183f5865b53decc19812e6
David H. Bronke <whitelynx@gmail.com>
2023-12-30 12:49:44 +0100

    Remapped pins again for some reason
    I forgot what drove this change, but this is the version I last tried on the STM32.

--- keyboards/handwired/dactyl_lynx/config.h
+++ keyboards/handwired/dactyl_lynx/config.h
@@ -28,8 +28,8 @@
 
 /* split connection */
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
-#define SERIAL_USART_TX_PIN A9     // USART TX pin
-#define SERIAL_USART_RX_PIN A10    // USART RX pin
+#define SERIAL_USART_TX_PIN B8     // USART TX pin
+#define SERIAL_USART_RX_PIN B9    // USART RX pin
 
 #define SELECT_SOFT_SERIAL_SPEED 1 // or 0, 2, 3, 4, 5
                                    //  0: 460800 baud
