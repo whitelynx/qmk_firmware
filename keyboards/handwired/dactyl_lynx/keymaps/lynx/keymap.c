5f4b44153fb25a87ece931d43ff367af97607983
David H. Bronke <whitelynx@gmail.com>
2024-01-02 02:52:59 +0100

    added QWERTY layer

--- keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
+++ keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
@@ -10,6 +10,7 @@
 #define MDIA 2 // media keys
 #define MOUS 3 // mouse keys
 #define NAV  4 // navigation keys
+#define QWRT 5 // QWERTY mode
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -145,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | 5clk | 4clk | 3clk | Rclk | Lclk | WhlU |                    |      |MsLeft|MsDown| MsUp |MsRght|      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      | WhlD |                    |      |      |      |      |      |      |
+ * |      |      |QWERTY|      |      | WhlD |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
  * |      |      |      |      |      |                                  | Left | Down |  Up  | Right|      |
  * `----------------------------------'                                  `----------------------------------'
@@ -164,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WH_L,  KC_WH_R,
        KC_BTN5,  KC_BTN4,  KC_BTN3,  KC_BTN2,  KC_BTN1,  KC_WH_U,
-       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WH_D,
+       KC_TRNS,  KC_TRNS,LTG(QWRT),  KC_TRNS,  KC_TRNS,  KC_WH_D,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                          KC_TRNS,  KC_TRNS,
                                                KC_TRNS,  KC_TRNS,
@@ -221,6 +222,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
+/* Keymap 5: Basic layer
+ *
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |   =  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Del  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | BkSp |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |; / L2|'/Cmd |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|Z/Ctrl|   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |//Ctrl|RShift|
+ * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
+ * |Grv/L1|  '"  |AltShf| Left | Right|                                  |  Up  | Down |   [  |   ]  | ~L1  |
+ * `----------------------------------'                                  `----------------------------------'
+ *                                      ,-------------.  ,-------------.
+ *                                      | Home | End  |  | PgDn | PgUp |
+ *                               ,------|------|------|  |------+------+------.
+ *                               | LGui | Space|      |  |      |Enter | RAlt |
+ *                               |------|------|Backsp|  |  Tab |------|------|
+ *                               | App  |      |ace   |  |      |      | ^/Esc|
+ *                               `--------------------'  `--------------------'
+ */
+[QWRT] = LAYOUT_dactyl(  // layer 0 : default
+        // left hand
+          KC_EQL,          KC_1,           KC_2,     KC_3,     KC_4,    KC_5,
+          KC_DEL,          KC_Q,           KC_W,     KC_E,     KC_R,    KC_T,
+          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,    KC_G,
+          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,    KC_B,
+  LT(SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,
+                                                                     KC_HOME,   KC_END,
+                                                            KC_LGUI,  KC_SPC,
+                                                      ALT_T(KC_APP),   KC_NO,  KC_BSPC,
+        // right hand
+                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
+                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
+                             KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
+                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
+                                    KC_UP,  KC_DOWN,  KC_LBRC,            KC_RBRC,          TT(SYMB),
+        KC_PGDN, KC_PGUP,
+                  KC_ENT, KC_RALT,
+         KC_TAB,   KC_NO, CTL_T(KC_ESC)
+    ),
 };
 
 void keyboard_post_init_user(void) {
--- keyboards/handwired/dactyl_lynx/keymaps/lynx/readme.md
+++ keyboards/handwired/dactyl_lynx/keymaps/lynx/readme.md
@@ -7,3 +7,5 @@
   * Notable differences:
     - Dvorak by default
     - My own layers, rather different from the original, and way more packed
+* Dec 2023:
+  * Added QWERTY layer
