e2922af3a566cae59f615068335e54bcd9e510d2
David H. Bronke <whitelynx@gmail.com>
2024-01-03 23:58:10 +0100

    full-duplex serial split works now

--- keyboards/handwired/dactyl_lynx/config.h
+++ keyboards/handwired/dactyl_lynx/config.h
@@ -8,10 +8,10 @@
 
 
 /* split connection */
-//#define SERIAL_USART_FULL_DUPLEX // Enable full duplex operation mode.
+#define SERIAL_USART_FULL_DUPLEX // Enable full duplex operation mode.
 //#define SERIAL_PIO_USE_PIO0
-//#define SERIAL_USART_TX_PIN GP0  // USART TX pin
-//#define SERIAL_USART_RX_PIN GP1  // USART RX pin
+#define SERIAL_USART_TX_PIN GP0  // USART TX pin
+#define SERIAL_USART_RX_PIN GP1  // USART RX pin
 
 //#define SERIAL_USART_DRIVER SIOD0
 
