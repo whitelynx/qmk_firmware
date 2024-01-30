1d1ec6c47e66c45546114eeb81b71bb11bbdc6c9
David H. Bronke <whitelynx@gmail.com>
2024-01-30 23:59:51 +0100

    Updated QWERTY layer

--- keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
+++ keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
@@ -222,46 +222,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
-/* Keymap 5: Basic layer
+/* Keymap 5: QWERTY layer
  *
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   =  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |  Esc |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Del  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |  Tab |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | BkSp |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |; / L2|'/Cmd |
+ * | LCtrl|   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|Z/Ctrl|   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |//Ctrl|RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
- * |Grv/L1|  '"  |AltShf| Left | Right|                                  |  Up  | Down |   [  |   ]  | ~L1  |
+ * | Menu |   `  |   =  |   [  |   ]  |                                  | Left | Down |  Up  | Right| RGui |
  * `----------------------------------'                                  `----------------------------------'
  *                                      ,-------------.  ,-------------.
- *                                      | Home | End  |  | PgDn | PgUp |
+ *                                      |      |      |  |      |      |
  *                               ,------|------|------|  |------+------+------.
- *                               | LGui | Space|      |  |      |Enter | RAlt |
- *                               |------|------|Backsp|  |  Tab |------|------|
- *                               | App  |      |ace   |  |      |      | ^/Esc|
+ *                               |      |      |      |  |      |      |      |
+ *                               |------|------|      |  |      |------|------|
+ *                               |      |      |      |  |      |      |      |
  *                               `--------------------'  `--------------------'
  */
 [QWRT] = LAYOUT_dactyl(  // layer 0 : default
-        // left hand
-          KC_EQL,          KC_1,           KC_2,     KC_3,     KC_4,    KC_5,
-          KC_DEL,          KC_Q,           KC_W,     KC_E,     KC_R,    KC_T,
-          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,    KC_G,
-          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,    KC_B,
-  LT(SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,
-                                                                     KC_HOME,   KC_END,
-                                                            KC_LGUI,  KC_SPC,
-                                                      ALT_T(KC_APP),   KC_NO,  KC_BSPC,
+       // left hand
+        KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,    KC_5,
+        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,
+       KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,    KC_G,
+       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,
+        KC_APP,   KC_GRV,   KC_EQL,  KC_LBRC,  KC_RBRC,
+                                                         KC_TRNS,  KC_TRNS,
+                                               KC_TRNS,  KC_TRNS,
+                                               KC_TRNS,  KC_TRNS,  KC_WBAK,
         // right hand
-                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
-                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
-                             KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
-                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
-                                    KC_UP,  KC_DOWN,  KC_LBRC,            KC_RBRC,          TT(SYMB),
-        KC_PGDN, KC_PGUP,
-                  KC_ENT, KC_RALT,
-         KC_TAB,   KC_NO, CTL_T(KC_ESC)
+                    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
+                    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_BSLS,
+                    KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
+                    KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
+                           KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_RGUI,
+       KC_TRNS,  KC_TRNS,
+                 KC_TRNS,  KC_TRNS,
+       KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 };
 
