b8a8e29451cef8e2199c64bb181a90576dceb43d
David H. Bronke <whitelynx@gmail.com>
2024-02-12 20:54:27 +0100

    dactyl_lynx: change QWERTY behavior to switching default layer, and added shift to the thumb clusters

--- keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
+++ keyboards/handwired/dactyl_lynx/keymaps/lynx/keymap.c
@@ -7,7 +7,7 @@
 static bool display_enabled;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: Basic layer (default)
  *
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  Esc |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  \   |
@@ -23,12 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               ,--------------------.  ,--------------------.
  *                               | LGui | LAlt |<MOUS>|  |<SYMB>| RAlt | RCtrl|
  *                               |------|------|------|  |------+------+------|
- *                               |      |  Del |<MDIA>|  |<NAV> |Enter |      |
+ *                               |LShift|  Del |<MDIA>|  |<NAV> |Enter |RShift|
  *                               |------|------'------|  |------'------|------|
  *                               |<QWRT>|  Backspace  |  |    Space    |      |
  *                               `--------------------'  `--------------------'
  */
-[BASE] = LAYOUT_dactyl(  // layer 0 : default
+[BASE] = LAYOUT_dactyl(
           // left hand
            KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,
            KC_TAB,  KC_QUOT,  KC_COMM,   KC_DOT,     KC_P,     KC_Y,
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_LSFT,  KC_SCLN,     KC_Q,     KC_J,     KC_K,     KC_X,
            KC_APP,   KC_GRV,   KC_EQL,  KC_LBRC,  KC_RBRC,
                                                   KC_LGUI,  KC_LALT, TT(MOUS),
-                                                    KC_NO,   KC_DEL, TT(MDIA),
-                                                LTG(QWRT),  KC_BSPC,
+                                                  KC_LSFT,   KC_DEL, TT(MDIA),
+                                                 DF(QWRT),  KC_BSPC,
           // right hand
                        KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_BSLS,
                        KC_F,     KC_G,     KC_C,     KC_R,     KC_L,  KC_SLSH,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,  KC_RSFT,
                               KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_RGUI,
          TT(SYMB),  KC_RALT,  KC_RCTL,
-          TT(NAV),   KC_ENT,    KC_NO,
+          TT(NAV),   KC_ENT,  KC_RSFT,
                      KC_SPC,    KC_NO
 ),
 /* Keymap 1: Symbol Layer
@@ -230,33 +230,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Menu |   `  |   =  |   [  |   ]  |                                  | Left | Down |  Up  | Right| RGui |
  * `----------------------------------'                                  `----------------------------------'
  *                               ,--------------------.  ,--------------------.
- *                               |      |      |      |  |      |      |      |
+ *                               | LGui | LAlt |<MOUS>|  |<SYMB>| RAlt | RCtrl|
  *                               |------|------|------|  |------+------+------|
- *                               |      |      |      |  |      |      |      |
+ *                               |LShift|  Del |<MDIA>|  |<NAV> |Enter |RShift|
  *                               |------|------'------|  |------'------|------|
- *                               |      |             |  |             |      |
+ *                               |<BASE>|  Backspace  |  |    Space    |      |
  *                               `--------------------'  `--------------------'
  */
-[QWRT] = LAYOUT_dactyl(  // layer 0 : default
+[QWRT] = LAYOUT_dactyl(
        // left hand
         KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,    KC_5,
         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,
        KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,    KC_G,
        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,
         KC_APP,   KC_GRV,   KC_EQL,  KC_LBRC,  KC_RBRC,
-                                               KC_TRNS,  KC_TRNS,  KC_TRNS,
-                                               KC_TRNS,  KC_TRNS,  KC_TRNS,
-                                               KC_TRNS,  KC_TRNS,
+                                               KC_LGUI,  KC_LALT, TT(MOUS),
+                                               KC_LSFT,   KC_DEL, TT(MDIA),
+                                              DF(BASE),  KC_BSPC,
         // right hand
                     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
                     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_BSLS,
                     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
                     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
                            KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_RGUI,
-       KC_TRNS,  KC_TRNS,  KC_TRNS,
-       KC_TRNS,  KC_TRNS,  KC_TRNS,
-                 KC_TRNS,  KC_TRNS
-    ),
+      TT(SYMB),  KC_RALT,  KC_RCTL,
+       TT(NAV),   KC_ENT,  KC_RSFT,
+                  KC_SPC,    KC_NO
+),
 };
 
 
